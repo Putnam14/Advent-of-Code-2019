fe0280d11a377405f956458ca40ef3a3f5dcd14f
Putnam14 <putnambr76@gmail.com>
2019-12-15 18:37:31 -0700

    Day three - Part one, still need to do some cleanup

--- src/com/bridgerputnam/Main.java
+++ src/com/bridgerputnam/Main.java
@@ -1,6 +1,7 @@
 package com.bridgerputnam;
 import com.bridgerputnam.dayOne.problemOne;
 import com.bridgerputnam.dayOne.problemTwo;
+import com.bridgerputnam.dayThree.DayThree;
 import com.bridgerputnam.dayTwo.ProblemOne;
 import com.bridgerputnam.dayTwo.ProblemTwo;
 
@@ -9,5 +10,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Day one solutions:\n   Problem one: %s\n   Problem two: %s\n", problemOne.solution(), problemTwo.solution());
         System.out.printf("Day two solution:\n   Problem one: %s\n   Problem two: %s\n", ProblemOne.solution(), ProblemTwo.solution());
+        System.out.printf("Day three solutions:\n   Problem one: %s\n", new DayThree().solutionOne());
     }
 }
